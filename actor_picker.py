1822c0ecd5d23d59ea76b590b64b2fbb65d248ce
Cubie <Cubiegamedev@gmail.com>
2019-12-21 13:56:19 +0100

    Changed up the algorithm

--- actor_picker.py
+++ actor_picker.py
@@ -1,11 +1,9 @@
 
-from time import time
 import sys
 
-# TODO: Use an enum everywhere
-class Player:
-    V = 0
-    M = 1
+class Gender:
+    Male = 0
+    Female = 1
 
 class Graph(object):
     def __init__(self) -> None:
@@ -20,126 +18,141 @@ class Graph(object):
         self.n = int(n)
         self.m = int(m)
 
-        # TODO: Consider storing ID to has on, rather than hashing on name
-
         actress_dict = {}
-        for _ in range(self.n):
+        for i in range(self.n):
             name = data.pop(0)
-            actress_dict[name] = Actress(name)
-        
+            actress_dict[name] = Actress(name, i)
+
         actor_dict = {}
-        for _ in range(self.n):
+        for i in range(self.n):
             name = data.pop(0)
-            actor_dict[name] = Actor(name)
+            actor_dict[name] = Actor(name, i)
 
         people_dict = {**actress_dict, **actor_dict}
 
-        movie_dict = {}
         for _ in range(self.m):
-            name = data.pop(0)
-            s = int(data.pop(0))
-            movie = Movie(name, s)
-            for _ in range(s):
-                person = people_dict[data.pop(0)]
-                movie.add_cast(person)
-                person.starred_in(movie)
-            movie_dict[name] = movie
+            #name = data.pop(0)
+            movie = Movie(data.pop(0))
+            for _ in range(int(data.pop(0))):
+                #person = people_dict[data.pop(0)]
+                movie.add_cast(people_dict[data.pop(0)])
+                #person.starred_in(movie)
+            for person in movie.female_cast:
+                person.costarred |= movie.male_cast
+            for person in movie.male_cast:
+                person.costarred |= movie.female_cast
+            #for person in movie.cast:
+            #    person.costarred |= {a for a in movie.cast if a.gender != person.gender}
 
         self.actresses = {*actress_dict.values()}
         self.actors    = {*actor_dict.values()}
-        self.movies    = {*movie_dict.values()}
 
-        for key in people_dict:
-            people_dict[key].prepare()
-            #print(key, "has costarred with", people_dict[key].costarred)
+        #self.people_dict = people_dict
 
+        #for key in people_dict:
+        #    people_dict[key].prepare()
+        
     def pick_winner(self):
-        return "Mark" if self._pick_winner((self.actresses, self.actors), Actress(""), 0) else "Veronique"
+        return "Mark" if self.match_all() else "Veronique"
 
-    def _pick_winner(self, people: "Tuple(Set(Actress), Set(Actor))", prev_picked: "Person", turn: int) -> int:
-        # TODO: Change turn and return to some enum
+    def match(self, actress):
+        for func in (lambda act: not act.match, lambda act: act.match):
+            for actor in actress.costarred:
+                if func(actor) and not actor.explored:
+                    actor.explored = True
+                    if actor.match is None or self.match(actor.match): 
+                        actor.match = actress
+                        actress.match = actor
+                        return True
+        return False
 
-        #print(turn, ["V", "M"][turn % 2])
+    def match_all(self): 
+        for actress in self.actresses:
+            for a in self.actors:
+                a.explored = False
+            if not self.match(actress):
+                return False
+        return True
 
-        # Turn % 2 = 0: Veronique's turn
-        # Turn % 2 = 1: Mark's turn
+    def plot(self):
+        import networkx as nx
+        import matplotlib.pyplot as plt
+        g = nx.DiGraph()
+        values = [v for v in self.people_dict.values() if v.costarred]
+        g.add_nodes_from([v.name for v in values])
+        color = []
+        for vertex in values:
+            g.add_edges_from([(vertex.name, edge.name) for edge in vertex.costarred if vertex.match != edge])
+            color += ["gray"] * len(vertex.costarred)
+            if vertex.match:
+                g.add_edge(vertex.name, vertex.match.name)
+                color[-1] = "red"
+        nx.draw(g, 
+                with_labels=True, 
+                node_color=['y' if vertex.gender == "M" else 'g' for vertex in values], 
+                edge_color=color,
+                pos = nx.drawing.layout.bipartite_layout(g, [v.name for v in values if v.gender == "M"]))
+        plt.draw()
+        plt.show()
 
-        #print(people)
-        #options = people[turn % 2]
-        #if turn > 0:
-        #    print(prev_picked.costarred)
-        #    print(people[turn % 2] & prev_picked.costarred)
-        #    options &= prev_picked.costarred
-
-        #print(options)
-        for option in (people[turn % 2] & prev_picked.costarred if turn > 0 else people[turn % 2]):
-            #print(" " * turn, ["V", "M"][turn % 2], "tries", option)
-            # TODO: Change to have less set operations
-            #       Perhaps add `option` to `option.costarred_...`
-            # If the current player always wins by choosing `option`, then return
-            if self._pick_winner((people[0] - {option}, people[1] - {option}),
-                                 option,
-                                 turn + 1) == turn % 2:
-                return turn % 2
-            
-        # If options is empty, or the winner regardless of our choice is the opponent, 
-        # we return the opponents' id.
-        return (turn + 1) % 2
-    
 class Movie(object):
-    def __init__(self, name: str, cast_size: int) -> None:
+    def __init__(self, name: str) -> None:
         super().__init__()
         self.name = name
-        self.cast = set()
-        self.cast_size = cast_size
+        self.male_cast = set()
+        self.female_cast = set()
     
     def add_cast(self, person: "Person") -> None:
-        self.cast.add(person)
+        if person.gender == Gender.Male:
+            self.male_cast.add(person)
+        else:
+            self.female_cast.add(person)
     
+    #def get_cast(self, gender: "Gender") -> "Set(Person)":
+    #    if gender == Gender.Male:
+    #        return self.female_cast
+    #    return self.male_cast
+
     def __repr__(self) -> str:
         return f"<|{self.name}, starring: {', '.join(map(str, self.cast))}|>"
 
 class Person(object):
-    def __init__(self, name: str, gender: str):
+    def __init__(self, name: str, _id: int, gender: "Gender"):
         super().__init__()
         self.name             = name
-        self.gender           = gender # "M" or "F"
-        self.starred          = set()
-        # TODO: Consider one set for costarred
-        #self.costarred_female = set()
-        #self.costarred_male   = set()
+        self.id               = _id
+        self.gender           = gender
+    #    self.starred          = set()
         self.costarred        = set()
+        self.match = None
     
-    def prepare(self) -> None:
-        self.costarred = set().union(*(movie.cast for movie in self.starred))
+    #def prepare(self) -> None:
+    #    self.costarred = set().union(*(movie.get_cast(self.gender) for movie in self.starred))
     
-    def starred_in(self, movie: "Movie") -> None:
-        self.starred.add(movie)
-        
-        # TODO: Make this more efficient:
-        #for person in movie.cast:
-        #    if person.gender == "F":
-        #        self.costarred_female.add(person)
-        #    else:
-        #        self.costarred_male.add(person)
-        #self.costarred |= movie.cast
+    #def starred_in(self, movie: "Movie") -> None:
+    #    self.starred.add(movie)
 
     def __hash__(self) -> int:
-        return hash(self.name)
+        return self.id
     
     def __repr__(self) -> str: 
         return f"<{self.name}, {self.gender}>"
 
 class Actress(Person):
-    def __init__(self, name: str):
-        super().__init__(name, "F")
+    def __init__(self, name: str, _id: int) -> None:
+        super().__init__(name, _id, Gender.Female)
 
 class Actor(Person):
-    def __init__(self, name: str):
-        super().__init__(name, "M")
+    def __init__(self, name: str, _id: int) -> None:
+        super().__init__(name, _id, Gender.Male)
+        self.explored = False
 
 if __name__ == "__main__":
+    #from time import time
+    #t = time()
     g = Graph()
     g.parse_input()
+    #print(f"\n{time() - t:8f}s input parsing")
+    #t = time()
     print(g.pick_winner())
-    
+    #print(f"{time() - t:8f}s algorithm time")
